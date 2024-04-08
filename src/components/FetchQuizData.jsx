417283844ed01a32cff5061f911d99f087aea18d
Dean <iamdeanwilson@gmail.com>
2024-04-07 22:06:16 -0500

    Quizzes now calculate score upon submission

--- src/components/FetchQuizData.jsx
+++ src/components/FetchQuizData.jsx
@@ -5,8 +5,31 @@ import {Radio, RadioGroup, FormControlLabel,FormControl, FormLabel, Button} from
 function FetchQuizData() {
 
     const { topic, difficulty } = useParams();
+    let questionPointValue
 
-    let [questions, setQuestions] = useState('');
+    if (difficulty === 'Easy'){
+      questionPointValue = 1;
+    } else if (difficulty === 'Medium'){
+      questionPointValue = 2;
+    } else if (difficulty === 'Hard'){
+      questionPointValue = 3;
+    }
+
+    const [questions, setQuestions] = useState('');
+    const [userScore, setUserScore] = useState(null);
+    const [backgroundColors, setBackgroundColors] = useState([]);
+    const [userCorrectAnswers, setUserCorrectAnswers] = useState(null);
+    const [userTimeTaken, setUserTimeTaken] = useState(null);
+    const [userTimeMultiplier, setUserTimeMultiplier] = useState(null);
+
+    let correctAnswers = [];
+    let userAnswers = [];
+    let backgroundColorArray=[]
+    let timeMultiplier; 
+    let score = 0;
+    let numberOfCorrectAnswers = 0;
+
+    let startTime = performance.now();
 
     useEffect(() => {
       fetch(`https://quizapi.io/api/v1/questions?apiKey=VsDMbtp8OFRwNTdLxnpFqtTpdkst98Mxw2tiOHHH&difficulty=${difficulty}&limit=10&tags=${topic}`)
@@ -15,36 +38,75 @@ function FetchQuizData() {
       })
       .then((data) => {
         setQuestions(data);
-      }) 
+        for(let i= 0 ; i < userAnswers.length ; i++ ){
+          userAnswers.push('');
+        };
+      })
     }, []);
   
-    function returnCorrectAnswer(){
-      if (question.correct_answers == "answer_a_correct"){
-        return "Answser A";
-      } else if (question.correct_answers == "answer_b_correct"){
-        return "Answser B";
-      } else if (question.correct_answers == "answer_c_correct"){
-        return "Answser C";
-      } else if (question.correct_answers == "answer_d_correct"){
-        return "Answser D";
-      } else if (question.correct_answers == "answer_e_correct"){
-        return "Answser E";
-      } else if (question.correct_answers == "answer_f_correct"){
-        return "Answser F";
+    const handleSubmit = () => {
+      let endTime = performance.now();
+      for(let i= 0 ; i < userAnswers.length ; i++ ){
+        if (userAnswers[i] === correctAnswers [i]){
+          backgroundColorArray.push("#52b202")
+          score = score + questionPointValue;
+          numberOfCorrectAnswers++
+        } else backgroundColorArray.push("#e3504b")
+      };
+      let quizTime = Math.floor((endTime - startTime)/1000);
+
+
+      if (quizTime <= 300 && quizTime > 270){
+        timeMultiplier = 2;
+      } else if (quizTime <= 270 && quizTime > 240){
+        timeMultiplier = 3;
+      } else if (quizTime <= 240 && quizTime > 210){
+        timeMultiplier = 4;
+      } else if (quizTime <= 210 && quizTime > 180){
+        timeMultiplier = 5;
+      } else if (quizTime <= 180 && quizTime > 150){
+        timeMultiplier = 6;
+      } else if (quizTime <= 150 && quizTime > 120){
+        timeMultiplier = 7;
+      } else if (quizTime <= 120 && quizTime > 90){
+        timeMultiplier = 8;
+      } else if (quizTime <= 90 && quizTime > 60){
+        timeMultiplier = 9;
+      } else if (quizTime <= 60 && quizTime > 60){
+        timeMultiplier = 10;
+      } else if (quizTime <= 60 && quizTime > 30){
+        timeMultiplier = 11;
+      } else if (quizTime <= 30){
+        timeMultiplier = 12;
       }
+      score = (score*timeMultiplier);
+      alert("Time: " + quizTime + " Seconds" + "\nNumber of Correct Answers: " + numberOfCorrectAnswers + "\nTime Multiplier: " + timeMultiplier + "\nScore: " + score);
+      setBackgroundColors(backgroundColorArray);
+      setUserCorrectAnswers(numberOfCorrectAnswers);
+      setUserTimeMultiplier(timeMultiplier);
+      setUserTimeTaken(quizTime);
+      setUserScore(score);
+      window.scrollTo(0, 0);
+      event.preventDefault(); 
     }
 
-    
     return (
       <div>
+        {userScore && <div style={{padding: '10px', borderRadius: '25px', margin : '5px', background:"#1565c0", color: "white"}}>
+          <h1>Your Score: {userScore} Points</h1>
+          <h3>Number of Correct Answers: {userCorrectAnswers}</h3>
+          <h3>Time Taken: {userTimeTaken} Seconds</h3>
+          <h3>Time Multiplier: {userTimeMultiplier}</h3>
+
+          </div>}
         <FormControl>
           {questions && <div>{questions.map((question, index) => (
-            <div style={{border: '5px solid rgba(0, 0, 0, 0.96)', padding: '10px', borderRadius: '25px', margin : '5px'}}>
+            <div style={{border: '5px solid rgba(0, 0, 0, 0.96)', padding: '10px', borderRadius: '25px', margin : '5px', background: backgroundColors[index]}}>
               <h3>Question {index +1}: {question.question}</h3 >
               <RadioGroup
                 aria-labelledby="demo-radio-buttons-group-label"
-                defaultValue="answer_a"
                 name="radio-buttons-group"
+                onChange={(event)=>(userAnswers[index] = event.target.value)}
               >
                 {question.answers.answer_a && <FormControlLabel value="answer_a" control={<Radio />} label={"A: " + question.answers.answer_a} style={{marginLeft: '30%'}} />}
                 {question.answers.answer_b && <FormControlLabel value="answer_b" control={<Radio />} label={"B: " + question.answers.answer_b} style={{marginLeft: '30%'}} />}
@@ -53,18 +115,24 @@ function FetchQuizData() {
                 {question.answers.answer_e && <FormControlLabel value="answer_e" control={<Radio />} label={"E: " + question.answers.answer_e} style={{marginLeft: '30%'}} />}
                 {question.answers.answer_f && <FormControlLabel value="answer_f" control={<Radio />} label={"F: " + question.answers.answer_f} style={{marginLeft: '30%'}} />}
               </RadioGroup>
-              <h3>Correct Anwswer:</h3>
-              {question.correct_answers.answer_a_correct === "true" && <h3>Answer A: "{question.answers.answer_a}"</h3>}
-              {question.correct_answers.answer_b_correct === "true" && <h3>Answer B: "{question.answers.answer_b}"</h3>}
-              {question.correct_answers.answer_c_correct === "true" && <h3>Answer C: "{question.answers.answer_c}"</h3>}
-              {question.correct_answers.answer_d_correct === "true" && <h3>Answer D: "{question.answers.answer_d}"</h3>}
-              {question.correct_answers.answer_e_correct === "true" && <h3>Answer E: "{question.answers.answer_e}"</h3>}
-              {question.correct_answers.answer_f_correct === "true" && <h3>Answer F: "{question.answers.answer_f}"</h3>}
+
+              {question.correct_answers.answer_a_correct === "true" && <script>function() {correctAnswers.push('answer_a')} </script>}
+              {question.correct_answers.answer_b_correct === "true" && <script>function() {correctAnswers.push('answer_b')} </script>}
+              {question.correct_answers.answer_c_correct === "true" && <script>function() {correctAnswers.push('answer_c')} </script>}
+              {question.correct_answers.answer_d_correct === "true" && <script>function() {correctAnswers.push('answer_d')} </script>}
+              {question.correct_answers.answer_e_correct === "true" && <script>function() {correctAnswers.push('answer_e')} </script>}
+              {question.correct_answers.answer_f_correct === "true" && <script>function() {correctAnswers.push('answer_f')} </script>}
+
+              {userScore && <h3>{"Correct Answer: Answer " + correctAnswers[index].charAt(7).toUpperCase()}</h3>}
+
             </div>
           ))}</div>}
-          <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="contained">
+          {!userScore && <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="contained" onClick={handleSubmit}>
             Submit Quiz!
-          </Button>
+          </Button>}
+          {userScore && <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="contained">
+            Add Score to Your Profile!
+          </Button>}
         </FormControl>
       </div>
     );
