bef3a20615b1f2d1905ca10d7d436f6d6bf1ef4e
Dean <iamdeanwilson@gmail.com>
2024-04-11 20:05:29 -0500

    Resolved a few more conflicts

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { Password } from '@mui/icons-material';
 import '../App.css';
   
-
 export default function Login(){
 
   const [username, setUsername] = React.useState('');
@@ -15,31 +14,8 @@ export default function Login(){
   const[usernameHelperText, setUsernameHelperText]=React.useState('')
   const[passwordHelperText, setPasswordHelperText]=React.useState('')
 
-  // const handleClick = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     //const response = await axios.post('/user/login', { username, password });
-  //      if (username === ''){
-  //       setUsernameError(true);
-  //       setUsernameHelperText("Username is required!");
-  //      } else if (password  === ''){
-  //       setPasswordError(true);
-  //       setPasswordHelperText("Password is required!");
-  //       event.preventDefault();
-  //      }
-  //     event.preventDefault();
-  //     fetch("http://localhost:8080/user/login", {
-  //     method:"POST",
-  //     headers:{"Content-Type":"application/json"},
-  //     body:JSON.stringify(user)
-  //   }).then(event =>  window.location.href='/')
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
   useEffect(() => {
+
     setUsernameError(false)
     setPasswordError(false)
     setUsernameHelperText('')
@@ -48,6 +24,14 @@ export default function Login(){
 
   const auth = useAuth();
   const handleClick = (e) => {
+    if (username === ''){
+        setUsernameError(true);
+        setUsernameHelperText("Username is required!");
+        } else if (password  === ''){
+        setPasswordError(true);
+        setPasswordHelperText("Password is required!");
+        event.preventDefault();
+        }
     e.preventDefault();
     const input={username, password}
     if (input.username !== "" && input.password !== "") {
@@ -79,6 +63,8 @@ export default function Login(){
           <TextField id="username" label="Username" variant="outlined" 
             value={username}
             onChange={(event)=>setUsername(event.target.value)}
+            error={usernameError}
+            helperText= {usernameHelperText}
             required
           />
         </div>
@@ -87,6 +73,8 @@ export default function Login(){
           <TextField type= "password" id="password" label="Password" variant="outlined" autoComplete="off" 
             value={password}  
             onChange={(event)=>setPassword(event.target.value)}
+            error={passwordError}
+            helperText= {passwordHelperText}
             required
           />
         </div>
@@ -98,46 +86,4 @@ export default function Login(){
         </div>
       </Box>
     );
-
-//   return (
-//     <Box
-//       component="form"
-//       sx={{
-//         '& > :not(style)': { m: 1, width: '25ch' },
-//       }}
-//       noValidate
-//       autoComplete="off"
-//       style={{border: '5px solid rgba(0, 0, 0, 0.96)', padding: '50px', borderRadius: '25px'}}
-//     >
-//       <div>
-//         <h2>Login</h2>
-//       </div>
-
-//       <div>
-//         <TextField id="username" label="Username" variant="outlined" 
-//           error={usernameError}
-//           helperText= {usernameHelperText}
-//           value={username}
-//           onChange={(event)=>setUsername(event.target.value)}
-//           required
-//         />
-//       </div>
-
-//       <div>
-//         <TextField type= "password" id="password" label="Password" variant="outlined" autoComplete="off" 
-//           error={passwordError}
-//           helperText= {passwordHelperText}
-//           value={password}  
-//           onChange={(event)=>setPassword(event.target.value)}
-//           required
-//         />
-//       </div>
-      
-//       <div>
-//         <Button variant="contained" onClick={handleClick}>
-//           Submit
-//         </Button>
-//       </div>
-//     </Box>
-//   );
 };
