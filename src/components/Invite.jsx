b63ae01d63ba3cbad1a963c5f98cd244b5b255b3
ngarner98 <exmachina091098@gmail.com>
2024-04-15 20:59:07 -0500

    Invite form

--- src/components/Invite.jsx
+++ src/components/Invite.jsx
@@ -1,86 +1,97 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser';
 import {Box, TextField, Stack, Button} from '@mui/material';
 
-function Invite() {
+const Invite = () => {
 
-  const[inviteEmail, setInviteEmail]=React.useState('');
-  const[message, setMessage]=React.useState('Come join and check out this website!' + '\n[INSERT URL HERE]');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleClick = (event) => {
-    event.preventDefault()
-    const invite = { inviteEmail, message };
-    if (inviteEmail === '' || message === '') {
-      alert("All fields are required!");
-      event.preventDefault();
-    } else if (message.length < 1) {
-      alert("Please provide an invite message!");
-      event.preventDefault();
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(inviteEmail)) {
-      alert("Invalid email address!");
-      event.preventDefault();
-    }
-    else
-      fetch("http://localhost:8080/Invite", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(invite)
+    // Our EmailJS service ID, template ID, and Public Key for this form
+    const serviceId = 'service_wsohobj';
+    const templateId = 'template_1817ib2';
+    const publicKey = 'JEEQ6sgoYE9S5a-cS';
+
+    // Object containing dynamic template parameters for this form
+    const templateParams = {
+      from_name: name,
+      recipient: email,
+      message: message,
+    };
+
+    // Handler that takes the parameters and emailJS data in order to send the email
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        alert(`Email sent successfully!`)
+        setName('');
+        setEmail('');
+        setMessage('');
       })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          alert("Invite sent!");
-          window.location.href = '/invite';
-        })
-        .catch(error => {
-          console.error('There was a problem with the fetch operation:', error);
-          alert("Error occurred while submitting the form. Please try again later.");
-        });
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        alert(`Error!`)
+      });
   }
-  
-  
+
   return (
-    <Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 7, width: '40ch' },
-    }}
-    noValidate
-    autoComplete="off"
-    style={{border: '5px solid rgba(, 0, 0, 0.96)', padding: '50px', borderRadius: '25px'}}>
-      <div>
-        <h2>Invite</h2>
-      </div>
-      <div>
-          <TextField id="inviteEmail" label="Email" variant="outlined"
-            fullWidth 
-            value={inviteEmail}
-            onChange={(event)=>setInviteEmail(event.target.value)} 
-            required
-          />
-      </div>
-      <div>
-          <TextField
-            id="message"
-            label="Message"
-            multiline
-            rows={5}
-            
-            value={message}
-            fullWidth
-            onChange={(event)=>setMessage(event.target.value)} 
-          />
-      </div>
-      <div>
-        <Button variant= "contained" onClick={handleClick}>
-          Send Invite
-        </Button> 
-      </div>
-    </Box>
-  );
+    //form that user inputs data into
+    <form onSubmit={handleSubmit} className='emailForm'>
+      <Box
+        sx={{
+          '& > :not(style)': { m: 7, width: '40ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        style={{border: '5px solid rgba(, 0, 0, 0.96)', padding: '50px', borderRadius: '25px'}}>
+          <div>
+            <h2>Invite</h2>
+          </div>
+          <div>
+              <TextField id="yourName" label="Name" variant="outlined"
+                fullWidth
+                type="text"
+                placeholder="Your Name" 
+                value={name}
+                onChange={(e) => setName(e.target.value)} 
+                required
+              />
+          </div>
+          <div>
+              <TextField id="inviteEmail" label="Email" variant="outlined"
+                fullWidth
+                type="text"
+                placeholder="Their Email" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} 
+                required
+              />
+          </div>
+          <div>
+              <TextField
+                id="message"
+                label="Message"
+                multiline
+                rows={5}
+                
+                value={message}
+                fullWidth
+                onChange={(e)=>setMessage(e.target.value)} 
+              />
+              
+          </div>
+          <div>
+            <Button 
+                type="submit">Send Invite
+            </Button> 
+          </div>
+        </Box>
+    </form>
+  )
 }
 
-export default Invite;
+export default Invite
